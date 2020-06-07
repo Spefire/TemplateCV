5fa2b6a56f703f1b7c1b1a741b51e79814837758
Nicholas Brun <spefire@gmail.com>
2020-06-07 02:21:14 +0200

    Feat(about): Prepare about section

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,11 +1,14 @@
 import Header from "@/components/header/Header.vue";
 
+import cvFile from "@/assets/files/CV.jpeg";
 /*import facebookIcon from "@/assets/icons/facebook.svg";
 import twitterIcon from "@/assets/icons/twitter.svg";
 import instagramIcon from "@/assets/icons/instagram.svg";
 import twitchIcon from "@/assets/icons/twitch.svg";*/
+import circleDownIcon from "@/assets/icons/circle-down.svg";
 import githubIcon from "@/assets/icons/github.svg";
 import linkedinIcon from "@/assets/icons/linkedin.svg";
+import profileImage from "@/assets/images/profile.png";
 
 export default {
   name: "Home",
@@ -18,8 +21,11 @@ export default {
       twitterIcon: twitterIcon,
       instagramIcon: instagramIcon,
       twitchIcon: twitchIcon,*/
+      circleDownIcon: circleDownIcon,
       githubIcon: githubIcon,
       linkedinIcon: linkedinIcon,
+      cvFile: cvFile,
+      profileImage: profileImage,
     };
   },
 };
