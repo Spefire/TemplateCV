dbdfcf308265b4899b2543d17db6f9b019cef37d
Nicholas Brun <spefire@gmail.com>
2020-06-08 13:35:02 +0200

    Feat(about): Add cv pdf file

--- src/views/about/About.ts
+++ src/views/about/About.ts
@@ -1,6 +1,5 @@
 import { Component, Vue } from "vue-property-decorator";
 
-import cvFile from "@/assets/files/CV.jpeg";
 import folderUploadIcon from "@/assets/icons/folder-upload.svg";
 import usersIcon from "@/assets/icons/users.svg";
 
@@ -8,7 +7,7 @@ import usersIcon from "@/assets/icons/users.svg";
 export default class About extends Vue {
   aboutText =
     "Avide d'aventures et de découvertes, j'ai toujours préféré apprendre par la pratique au sein d'entreprises afin que ces nouvelles connaissances soient utiles à des projets immédiats. Je souhaite aujourd'hui, valider ces connaissances, sans cesser d'apprendre, par le biais d'une formation en alternance.";
-  cvFile = cvFile;
+  cvFile = "./CV_Cecile_Blin.pdf";
   folderUploadIcon = folderUploadIcon;
   usersIcon = usersIcon;
 }
