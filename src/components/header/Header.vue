9052e2705c091ca4f90403c7228997a92ae4685f
Nicholas Brun <spefire@gmail.com>
2020-06-08 18:22:54 +0200

    Feat(header): Add dynamic color about the sections

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -4,13 +4,42 @@
     v-bind:style="{ opacity: fixNavbar ? '1.0' : opacNavbar }"
   >
     <nav>
-      <div v-scroll-to="{ el: '#nav-home', offset: 0 }">Accueil</div>
-      <div v-scroll-to="{ el: '#nav-about', offset: 0 }">Présentation</div>
-      <div v-scroll-to="{ el: '#nav-education', offset: -50 }">
-        Formations et Expériences
+      <div
+        v-scroll-to="{ el: '#nav-home', offset: 0 }"
+        v-bind:class="{ active: currentLocation === 'nav-home' }"
+      >
+        Accueil
       </div>
-      <div v-scroll-to="{ el: '#nav-skills', offset: -50 }">Compétences</div>
-      <div v-scroll-to="{ el: '#nav-contact', offset: -50 }">Contact</div>
+      <div
+        v-scroll-to="{ el: '#nav-about', offset: 0 }"
+        v-bind:class="{ active: currentLocation == 'nav-about' }"
+      >
+        Présentation
+      </div>
+      <div
+        v-scroll-to="{ el: '#nav-education', offset: -50 }"
+        v-bind:class="{ active: currentLocation == 'nav-education' }"
+      >
+        Formations
+      </div>
+      <div
+        v-scroll-to="{ el: '#nav-experiences', offset: -50 }"
+        v-bind:class="{ active: currentLocation == 'nav-experiences' }"
+      >
+        Expériences
+      </div>
+      <div
+        v-scroll-to="{ el: '#nav-skills', offset: -50 }"
+        v-bind:class="{ active: currentLocation == 'nav-skills' }"
+      >
+        Compétences
+      </div>
+      <div
+        v-scroll-to="{ el: '#nav-contact', offset: -50 }"
+        v-bind:class="{ active: currentLocation == 'nav-contact' }"
+      >
+        Contact
+      </div>
     </nav>
   </header>
 </template>
